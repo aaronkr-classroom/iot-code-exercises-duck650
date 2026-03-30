65b6f550c150a9b66dae95b5b5bb5e23a6ff9d65
duck650 <thkim1044@gmail.com>
2026-03-30 10:01:57 +0900

    Add files via upload

---
+++ code/3장/3_bool.py
@@ -0,0 +1,12 @@
+print(3 > 1)   # True
+print(5 == 2)  # False
+print(4 != 4)  # False
+
+print(bool(1))     # True
+print(bool(0))     # False
+print(bool(""))    # False
+print(bool("hi"))  # True
+
+num = 6
+
+print(num % 2 == 0)  # True (짝수)
